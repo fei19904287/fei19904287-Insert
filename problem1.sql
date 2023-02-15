c9632bc37129d0589627ae92783f2d1fb1564d81
fei19904287 <feili4287@gmail.com>
2023-02-15 00:48:18 +0000

    SQL insert into

--- problem1.sql
+++ problem1.sql
@@ -1,1 +1,3 @@
-write your sql statement in a single line in the problem1.sql file.
+INSERT INTO song (title, artist) VALUES ('Let it be', 'Beatles' );
+INSERT INTO song (title, artist) VALUES ('Hotel California','Eagles');
+INSERT INTO song (title, artist) VALUES ('Kashmir' , 'Led Zeppelin' );
